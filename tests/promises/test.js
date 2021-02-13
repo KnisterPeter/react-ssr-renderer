d1f4693352669e0936832861f898f25c74b7207a
Markus Wolf <mail@markus-wolf.de>
2021-02-13 16:32:29 +0100

    test: add more wrapped promise cases

--- tests/promises/test.js
+++ tests/promises/test.js
@@ -5,7 +5,7 @@ test("FakePromise should wrap new Promise", async () => {
 
   new WrappedPromise((resolve) => resolve());
 
-  const active = WrappedPromise.activePromises;
+  const active = WrappedPromise.retrieveAndClearActivePromises();
   expect(active).toHaveLength(1);
 
   await Promise.all(active);
@@ -16,7 +16,7 @@ test("FakePromise should wrap next then-ed promise", async () => {
 
   new WrappedPromise((resolve) => resolve()).then(() => {});
 
-  const active = WrappedPromise.activePromises;
+  const active = WrappedPromise.retrieveAndClearActivePromises();
   expect(active).toHaveLength(2);
 
   await Promise.all(active);
@@ -31,7 +31,7 @@ test("FakePromise should wrap next catch-ed promise", async () => {
     })
     .catch(() => {});
 
-  const active = WrappedPromise.activePromises;
+  const active = WrappedPromise.retrieveAndClearActivePromises();
   expect(active).toHaveLength(3);
 
   expect(Promise.all(active)).rejects.toThrow();
@@ -53,8 +53,41 @@ test("FakePromise should wrap Promise.all", async () => {
     })
     .then(() => 123);
 
-  const active = WrappedPromise.activePromises;
+  const active = WrappedPromise.retrieveAndClearActivePromises();
   expect(active).toHaveLength(5);
+
+  await Promise.all(active);
+});
+
+test("FakePromise should wrap Promise.resolve", async () => {
+  const WrappedPromise = fakePromiseFactory(Promise);
+
+  WrappedPromise.resolve();
+
+  const active = WrappedPromise.retrieveAndClearActivePromises();
+  expect(active).toHaveLength(1);
+
+  await Promise.all(active);
+});
+
+test("FakePromise should wrap Promise.reject", async () => {
+  const WrappedPromise = fakePromiseFactory(Promise);
+
+  WrappedPromise.reject(1);
+
+  const active = WrappedPromise.retrieveAndClearActivePromises();
+  expect(active).toHaveLength(1);
+
+  expect(Promise.all(active)).rejects.toBe(1);
+});
+
+test("FakePromise should wrap Promise.finally", async () => {
+  const WrappedPromise = fakePromiseFactory(Promise);
+
+  WrappedPromise.resolve().finally();
+
+  const active = WrappedPromise.retrieveAndClearActivePromises();
+  expect(active).toHaveLength(2);
 
   await Promise.all(active);
 });
