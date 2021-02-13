85625a523967d79265952b3331faa152c876d8e3
Markus Wolf <mail@markus-wolf.de>
2021-02-13 14:59:07 +0100

    fix: update types

--- types/fake-promise.d.ts
+++ types/fake-promise.d.ts
@@ -6,12 +6,73 @@ declare function fakePromiseFactory(NativePromise: typeof Promise): {
     new (task: () => unknown): {};
     /** @type {Promise<unknown>[]} */
     activePromises: Promise<unknown>[];
+    retrieveAndClearActivePromises(): Promise<unknown>[];
+    /**
+     * @param {Promise<any>} p
+     */
+    wrap(p: Promise<any>): {
+        /**
+         * @param {any[]} args
+         */
+        then(...args: any[]): any;
+        /**
+         * @param {any[]} args
+         */
+        catch(...args: any[]): any;
+        /**
+         * @param {any[]} args
+         */
+        finally(...args: any[]): any;
+    };
     /**
      * @param {unknown} value
      */
-    resolve(value: unknown): Promise<unknown>;
+    resolve(value: unknown): {
+        /**
+         * @param {any[]} args
+         */
+        then(...args: any[]): any;
+        /**
+         * @param {any[]} args
+         */
+        catch(...args: any[]): any;
+        /**
+         * @param {any[]} args
+         */
+        finally(...args: any[]): any;
+    };
     /**
      * @param {unknown} value
      */
-    reject(value: unknown): Promise<never>;
+    reject(value: unknown): {
+        /**
+         * @param {any[]} args
+         */
+        then(...args: any[]): any;
+        /**
+         * @param {any[]} args
+         */
+        catch(...args: any[]): any;
+        /**
+         * @param {any[]} args
+         */
+        finally(...args: any[]): any;
+    };
+    /**
+     * @param {Promise<any>[]} promises
+     */
+    all(promises: Promise<any>[]): {
+        /**
+         * @param {any[]} args
+         */
+        then(...args: any[]): any;
+        /**
+         * @param {any[]} args
+         */
+        catch(...args: any[]): any;
+        /**
+         * @param {any[]} args
+         */
+        finally(...args: any[]): any;
+    };
 };
