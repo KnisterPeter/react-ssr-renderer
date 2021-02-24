33fcee5887a8d4dc82614bb444352fab5ff8db61
Markus Wolf <mail@markus-wolf.de>
2021-02-24 14:34:07 +0100

    refactor: restructure types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,18 +1,33 @@
 /// <reference types="jsdom/ts3.4" />
+export type DOMResourceLoader = import('jsdom').ResourceLoader;
+export type DOMWinnow = import('jsdom').JSDOM['window'];
+export type FetchOptions = import('jsdom').FetchOptions;
+export type VMContext = import('vm').Context;
+export type SetupGlobalsFunction = (window: DOMWinnow) => void;
+export type ResourceLoaderFunction = (url: string, options: FetchOptions) => Promise<string> | null;
 import { Renderer } from "./renderer";
+/**
+ * @typedef {import('jsdom').ResourceLoader} DOMResourceLoader
+ * @typedef {import('jsdom').JSDOM['window']} DOMWinnow
+ * @typedef {import('jsdom').FetchOptions} FetchOptions
+ * @typedef {import('vm').Context} VMContext
+ *
+ * @typedef {(window: DOMWinnow) => void} SetupGlobalsFunction
+ * @typedef {(url: string, options: FetchOptions) => Promise<string> | null} ResourceLoaderFunction
+ */
 /**
  * @param {object} options
  * @param {string} options.html
  * @param {string} options.url
- * @param {((window: import('jsdom').JSDOM['window']) => void)=} options.setupGlobals
- * @param {((url: string, options: import('jsdom').FetchOptions) => Promise<string> | null)=} options.resourceLoader
+ * @param {SetupGlobalsFunction=} options.setupGlobals
+ * @param {ResourceLoaderFunction=} options.resourceLoader
  * @param {(typeof window['fetch'])=} options.fetch
  */
 export function render({ html, url, setupGlobals, resourceLoader, fetch }: {
     html: string;
     url: string;
-    setupGlobals?: ((window: import('jsdom').JSDOM['window']) => void) | undefined;
-    resourceLoader?: ((url: string, options: import('jsdom').FetchOptions) => Promise<string> | null) | undefined;
+    setupGlobals?: SetupGlobalsFunction | undefined;
+    resourceLoader?: ResourceLoaderFunction | undefined;
     fetch?: (typeof window['fetch']) | undefined;
 }): Promise<JSDOM>;
 import { JSDOM } from "jsdom";
